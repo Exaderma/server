ebe7aeaa38b7974d1e7e8432578fa322d43647e0
Lucaas <lucas.soury@epitech.eu>
2023-05-05 16:59:32 +0200

    [FIX] symfony application version 5.1 -> 6.1.99

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Entity\PatientTableEntity;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+
 class TestController
 {
     public function index()
@@ -12,6 +17,29 @@ class TestController
     public function test()
     {
         $age = $_GET['age'];
+        
         dd("test, vous avez $age ans");
+    }
+
+    public function insert(ManagerRegistry $doctrine, Request $request)
+    {
+        $entityManager = $doctrine.getManager();
+        $db = new PatientTableEntity();
+        $db->setFirstName("test");
+        $db->setLastName("test");
+        $db->setEmail("pipi.caca@prout.ff");
+        $db->setPassword("test");
+        $db->setAdmin(false);
+        $db->setCreatedAt(new \DateTime());
+        $entityManager->persist($db);
+        $entityManager->flush();
+        dd("insert");
+    }
+    
+    public function get(ManagerRegistry $doctrine, Request $request)
+    {
+        $entityManager = $doctrine.getManager();
+        $db = $entityManager->getRepository(PatientTableEntity::class)->findAll();
+        dd($db);
     }
 }
