c393d1fb3ee16aa6d95e3996f7cfb4a3da902ff3
Lucaas <lucas.soury@epitech.eu>
2023-05-11 10:44:06 +0200

    [ADD] error check unit tests

--- tests/Controller/PatientAuthentification/Register/RegisterTest.php
+++ tests/Controller/PatientAuthentification/Register/RegisterTest.php
@@ -3,14 +3,67 @@
 namespace App\Tests\Controller\PatientAuthentification\Register;
 
 use App\Repository\PatientTableRepository;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class RegisterTest extends KernelTestCase
+class RegisterTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testRegisterMissingParameter()
     {
-        $kernel = self::bootKernel();
-        $users = $kernel->getContainer()->get(PatientTableRepository::class)->count([]);+
-        $this->assertEquals(1, $users);
+        $client = static::createClient();
+        $crawler = $client->request('POST', '/patient/register', [], [], ['CONTENT_TYPE' => 'application/json'],
+            '{
+                "firstName": "Lucas",
+                "lastName": "Soury",
+                "email": "lucas.soury@oragnge.fr"
+            }'
+        );
+        $this->assertResponseStatusCodeSame(400);
     }
+
+    public function testRegisterMissingJSON()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', '/patient/register', [], [], ['CONTENT_TYPE' => 'application/json'],
+            '{}'
+        );
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    // public function testRegisterUserAlreadyExists()
+    // {
+    //     $client = static::createClient();
+
+    //     $client->request('POST', '/patient/register', [], [], ['CONTENT_TYPE' => 'application/json'],
+    //         '{
+    //             "firstName": "Lucas",
+    //             "lastName": "Soury",
+    //             "email": "lucas.soury@exists.fr",
+    //             "password": "password"
+    //         }'
+    //     );
+    //     $client->request('POST', '/patient/register', [], [], ['CONTENT_TYPE' => 'application/json'],
+    //         '{
+    //             "firstName": "Lucas",
+    //             "lastName": "Soury",
+    //             "email": "lucas.soury@exists.fr",
+    //             "password": "password"
+    //         }'
+    //     );
+
+    //     $this->assertResponseStatusCodeSame(409);
+    // }
+
+    // public function testRegisterWorkingAsIntended()
+    // {
+    //     $client = static::createClient();
+    //     $crawler = $client->request('POST', '/patient/register', [], [], ['CONTENT_TYPE' => 'application/json'],
+    //         '{
+    //             "firstName": "Lucas",
+    //             "lastName": "Soury",
+    //             "email": "lucas.soury@oragnge.fr",
+    //             "password": "password"
+    //         }'
+    //     );
+    //     $this->assertResponseStatusCodeSame(200);
+    // }
 }
