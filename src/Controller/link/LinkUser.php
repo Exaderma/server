7e36438b01c451ad95442204a9f40131220320a7
mistercouscous <erwan-baillon@orange.fr>
2023-06-10 15:49:32 +0200

    ADD prelink user

--- src/Controller/link/LinkUser.php
+++ src/Controller/link/LinkUser.php
@@ -6,6 +6,8 @@ use App\Entity\PatientTableEntity;
 use App\Entity\ProfessionalTableEntity;
 use App\Entity\LinkUserTableEntity;
 
+use App\Utils\guidv4;
+
 use Doctrine\Persistence\ManagerRegistry;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -22,23 +24,21 @@ class LinkUser
         $entityManager = $doctrine->getManager();
         $body = json_decode($request->getContent(), true);
         $patientId = $body['patientId'];
-        $doctorId = $body['doctorId'];
+        
+        return new Response(json_encode($body), Response::HTTP_OK);
+    }
 
-        $patient = $entityManager->getRepository(PatientTableEntity::class)->findOneBy(['id' => $patientId]);
-        $doctor = $entityManager->getRepository(ProfessionalTableEntity::class)->findOneBy(['id' => $doctorId]);
+    public function generateCode(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $doctors = $entityManager->getRepository(ProfessionalTableEntity::class)->findBy(['id' => 1]);
 
-        if ($patient && $doctor) {
-            $link = new LinkUserTableEntity();
-            $link->setPatientId($patientId);
-            $link->setDoctorId($doctorId);
+        $code = guidv4();
+        $doctors->setCode($code);
+        $entityManager->persist($doctors);
+        $entityManager->flush();
 
-            $entityManager->persist($link);
-            $entityManager->flush();
-
-            return new Response('Link created', Response::HTTP_OK);
-        }
-
-        return new Response('Link not created', Response::HTTP_BAD_REQUEST);
+        return new Response(json_encode($code), Response::HTTP_OK);
     }
 
     public function getLink(ManagerRegistry $doctrine): Response
@@ -48,10 +48,10 @@ class LinkUser
 
         $response = [];
         foreach ($links as $link) {
+            $patient = $entityManager->getRepository(PatientTableEntity::class)->find($link->getPatientId());
             $response[] = [
                 'id' => $link->getId(),
-                'patientId' => $link->getDoctorId(),
-                'doctorId' => $link->getPatientId(),
+                'patient' => $patient->getEmail(),
             ];
         }
 
