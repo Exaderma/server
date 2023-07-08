2fb2b8dc7c49b1436dd56e507c49730be64973fe
mistercouscous <erwan-baillon@orange.fr>
2023-07-08 14:45:55 +0200

    UPDATE test link resolver

--- test/offline/link/link.test.ts
+++ test/offline/link/link.test.ts
@@ -1,19 +1,21 @@
 import { resolverLinkDoctorToPatient, resolverLinkPatientToDoctor } from "../../../src/link/api/resolver";
 
 const templateResolverLink = {
-  LinkPatientToDoctor: async (patientId: string, doctorId: string) => "success",
-  LinkDoctorToPatient: async (doctorId: string, patientId: string) => "success",
+  LinkPatientToDoctor: async (code : number, patientId : number) => ("success"),
+  LinkDoctorToPatient: async (doctorId: number, email: string) => ("success"),
+  getLinkPatient: async (patientId: number) => 'success',
+  getLinkDoctor: async (doctorId: number) => 'success',
 };
 
 describe("link test", () => {
   test("link patient to doctor", async () => {
     const RepositoryLink = {
       ...templateResolverLink,
-      LinkPatientToDoctor: async (patientId: string, doctorId: string) =>
+      LinkPatientToDoctor: async (code : number, patientId : number) =>
         "success",
     };
 
-    const res = await resolverLinkPatientToDoctor(RepositoryLink);
+    const res = await resolverLinkPatientToDoctor(RepositoryLink, 1, 1);
 
     expect(res).toBe("success");
   });
@@ -21,11 +23,11 @@ describe("link test", () => {
   test("link doctor to patient", async () => {
     const RepositoryLink = {
       ...templateResolverLink,
-      LinkDoctorToPatient: async (doctorId: string, patientId: string) =>
+      LinkDoctorToPatient: async (doctorId: number, email: string) =>
         "success",
     };
 
-    const res = await resolverLinkDoctorToPatient(RepositoryLink);
+    const res = await resolverLinkDoctorToPatient(RepositoryLink, 1, "test@test.com");
 
     expect(res).toBe("success");
   });
